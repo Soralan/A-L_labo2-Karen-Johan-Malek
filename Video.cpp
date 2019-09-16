50dbcc1bb5115e07c260ea2b48548e6c2bfb1f74
Karenoreau <karenoreau@hotmail.com>
2019-09-16 16:43:26 -0400

    first commit

---
+++ Video.cpp
@@ -0,0 +1,27 @@
+#include "Video.h"
+
+
+
+Video::Video()
+{
+}
+
+
+Video::~Video()
+{
+}
+
+HRESULT Video::playPause()
+{
+	return E_NOTIMPL;
+}
+
+HRESULT Video::avanceRapide()
+{
+	return E_NOTIMPL;
+}
+
+HRESULT Video::retourDepart()
+{
+	return E_NOTIMPL;
+}
